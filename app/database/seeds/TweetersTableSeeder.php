87a2b841dfc1a3046cce8789ae74b7f058b4ce0a
Bowen Lu <bluinc@ubuntu.(none)>
2014-02-16 17:36:27 -0500

    no-changes

--- app/database/seeds/TweetersTableSeeder.php
+++ app/database/seeds/TweetersTableSeeder.php
@@ -7,7 +7,7 @@ class TweetersTableSeeder extends Seeder
     {
         DB::table('tweeters')->delete();
 
-        if (($handle = fopen("tweeters.csv", "r")) !== FALSE) {
+        if (($handle = fopen("/tweeters.csv", "r")) !== FALSE) {
             
             while ($line = fgetcsv($handle)) {
                 Tweeter::create(array(
