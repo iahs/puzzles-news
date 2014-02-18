73b00b8e30191f2dd9eeaf6b97d003d8d7ed681f
Zezhou Liu <zezhouliu@college.harvard.edu>
2014-02-18 07:23:06 -0800

    added rss table

---
+++ app/database/migrations/2014_02_18_150851_create_rss_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRssTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('rss_feeds', function(Blueprint $table) {
+			$table->string('title');
+			$table->string('website');
+			$table->string('permalink');
+			$table->integer('tag_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
