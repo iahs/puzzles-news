4659c13767c01b0b3d0dfb37e75429d7a5276547
Zezhou Liu <zezhouliu@college.harvard.edu>
2014-02-18 08:38:02 -0800

    Removed the author_id foreign key because it is run during migration before author table created

--- app/database/migrations/2014_02_18_151109_merge_posts_table.php
+++ app/database/migrations/2014_02_18_151109_merge_posts_table.php
@@ -17,7 +17,6 @@ class MergePostsTable extends Migration {
             $table->boolean('media');
             $table->integer('feed_id');
             $table->integer('author_id');
-            $table->foreign('author_id')->references('id')->on('authors');
             $table->foreign('feed_id')->references('id')->on('rss_feeds');
 		});
 	}
@@ -33,9 +32,7 @@ class MergePostsTable extends Migration {
         Schema::table('posts', function($table)
         {
             $table->dropForeign('feed_id');
-            $table->dropForeign('author_id');
             $table->dropColumn('media');
-            $table->dropColumn('author_id');
         });
 	}
 
