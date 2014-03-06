f2a26dd0b8b3b7c6e590e682e7aad024b939717a
iahs <iahs@users.noreply.github.com>
2014-03-05 20:37:51 -0500

    fix integrity issue with seed data

---
+++ app/database/migrations/2014_03_05_203134_trim_tweets.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class TrimTweets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function ($table) {
+            $table->dropColumn('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tags', function ($table) {
+            $table->string('description');
+        });
+    }
+
+}
