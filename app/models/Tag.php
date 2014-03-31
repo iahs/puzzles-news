63d033ec65b56803ac96b2fd3ebcf58b8ce78c5b
iahs <iahs@users.noreply.github.com>
2014-03-31 16:23:51 -0400

    Get only active tags

--- app/models/Tag.php
+++ app/models/Tag.php
@@ -15,4 +15,15 @@ class Tag extends Eloquent {
     {
         return $this->belongsToMany('Tweet');
     }
+
+    public function getAllWithPosts()
+    {
+        $tagObjects = DB::table("tag_post")->distinct()->get();
+        // Convert array of stdObj to an integer array
+        $tagIds = [];
+        foreach ($tagObjects as $tag) {
+            array_push($tagIds, $tag->tag_id);
+        }
+        return Tag::whereIn('id', $tagIds)->get();
+    }
 }
